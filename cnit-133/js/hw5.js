8585f4440b38160050814b1d117f971b5354d092
Truong <trnguyen34@gmail.com>
2023-10-28 12:51:15 -0700

    finished with the functionaliy

--- cnit-133/js/hw5.js
+++ cnit-133/js/hw5.js
@@ -1,6 +1,18 @@
+var errorArray = [];
+
 function run() {
-    document.getElementById('result').value = getCheckedBoxData() + " \n" 
-        + getRadioButtonData() + " \n" + getDropDownData() + " \n" + getUserFullName();
+    var name = getUserFullName();
+    var ageGroup = getRadioButtonData();
+    var browser = getCheckedBoxData();
+    var movieType = getDropDownData();
+
+    if (errorArray.length > 0) {
+        document.getElementById('result').value = errorArray.join("\n");
+    } else {
+        document.getElementById('result').value = "Thanks, your data was submitted!";
+    }
+
+    errorArray = [];
 }
 
 function getCheckedBoxData() {
@@ -14,11 +26,15 @@ function getCheckedBoxData() {
         }
     }
 
+    if (values.length === 0) {
+        errorArray.push("Please select least one browser.");
+    }
+
     return values;
 }
 
 function getRadioButtonData() {
-    var value;
+    var value = "";
     var select = document.getElementById("radio-button");
     var input = select.getElementsByTagName("input");
 
@@ -29,18 +45,46 @@ function getRadioButtonData() {
         }
     }
 
+    if (value.length === 0) {
+        errorArray.push("Please select your age group.");
+    }
+
     return value;
 }
 
 function getDropDownData() {
+    var value = "";
     var select = document.getElementById("movie-types");
-    return select.value;
+    if (!(select.value === "None")) {
+        return value = select.value;
+    } else {
+        errorArray.push("Please select a movie type.");
+        return;
+    }
 }
 
 function getUserFullName() {
-    return document.forms["survey-form"].elements["fullname"].value;
+    var fullName =  document.forms["survey-form"].elements["fullname"].value;
+
+    if (fullName.length === 0) {
+        return errorArray.push("Please enter your full name.");
+        
+    }
+
+    if (validateName(fullName)) {
+        return fullName;
+    }
+
+    return errorArray.push("Please enter your full name.");
 }
 
+function validateName(fullName) {
+    var regex = /^[a-zA-Z]+ [a-zA-Z]+$/;
+    if(!regex.test(fullName)) {
+        return false;
+    }
 
+    return true;
+}
 
 
